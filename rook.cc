dca6ec5b15bcf10dc1bfcd566edaa10395bda614
hyeonjeongkwon <105447603+hyeonjeongkwon@users.noreply.github.com>
2022-11-29 22:31:55 -0500

    Update rook.cc

--- rook.cc
+++ rook.cc
@@ -4,6 +4,8 @@
 Rook::Rook(Color color, int row, int col, bool isFirstMove) : color{color}, row{row}, col{col}, isFirstMove{false} {}
 
 isValidMove(int initialRow, int initialCol, int finalRow, int finalCol) {
+    if (finalRow >= 8) return false;
+    if (finalCol >= 8) return false;
     // if (initialRow == finalRow && initialCol == finalCol)
     if (initialRow == finalRow) return true; //move horizontally
     if (initialCol == finalCol) return true; //move vertically

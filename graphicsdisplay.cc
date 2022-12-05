2ee922153a06026a518ed842e91afcf96dec5961
j54jung <j54jung@uwaterloo.ca>
2022-12-04 23:05:45 -0500

    Graphics: Move and Castling Fixed

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -40,45 +40,62 @@ void GraphicsDisplay::notify() {
 		to_x = board->getTotalMoves().back().to.first;
                 to_y = board->getTotalMoves().back().to.second;
 	}
+	std::cout << from_x << from_y << std::endl;
+	std::cout << to_x << to_y << std::endl;
 	for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
                         if (board->getBoard()[i][j] != nullptr) { 
 				Colour c = board->getBoard()[i][j]->getColour();
-				if (from_x && from_y) {
+				if (board->getTotalMoves().size()) {
 					if ((from_x+from_y) % 2) {
 						w->fillRectangle(50*(from_y+1), 50*from_x, 50, 50, 1);
 					} else {
 						w->fillRectangle(50*(from_y+1), 50*from_x, 50, 50, 0);
 					}
 				}
-			/*	
-				if (to_x && to_y) {
+		/*	
+				if (board->getTotalMoves().size()) {
                                                 if ((to_x+to_y) % 2) {
-                                                        w->fillRectangle(50*(to_y), 50*to_x, 50, 50, 1);
+                                                        w->fillRectangle(50*(to_y+1), 50*to_x, 50, 50, 1);
                                                 } else {
                                                         w->fillRectangle(50*(to_y+1), 50*to_x, 50, 50, 0);
                                                 }
                                         }
-					*/
+		*/			
 				if (c == Colour::Black) {
+					if (to_y == j && to_x == i){
 					if ((i+j) % 2) {
                                                         w->fillRectangle(50*(j+1), 50*i, 50, 50, 1);
                                                 } else {
                                                     	w->fillRectangle(50*(j+1), 50*i, 50, 50, 0);
                                                 }
-
+					}
 					w->drawBigString(65+j*50, i*50+35, printPiece(board->getBoard()[i][j]->getType()), 4);
 				} else {
-                                		if ((i+j) % 2) {
+					if (to_y == j && to_x == i){
+                                  		if ((i+j) % 2) {
                                         		w->fillRectangle(50*(j+1), 50*i, 50, 50, 1);
                                 		} else {
                                         		w->fillRectangle(50*(j+1), 50*i, 50, 50, 0);
                                 		}
+						}
 					w->drawBigString(65+j*50, i*50+35, printPiece(board->getBoard()[i][j]->getType()), 3);
 				}
 
 
 			}
+			if (board->getBoard()[0][0] == nullptr) {
+				w->fillRectangle(50*(0+1), 50*0, 50, 50, 0);
+			}
+			if (board->getBoard()[7][7] == nullptr) {
+                                w->fillRectangle(50*(7+1), 50*7, 50, 50, 0);
+                        }
+			if (board->getBoard()[0][7] == nullptr) {
+                                w->fillRectangle(50*(7+1), 50*0, 50, 50, 1);
+                        }
+			if (board->getBoard()[7][0] == nullptr) {
+                                w->fillRectangle(50*(0+1), 50*7, 50, 50, 1);
+                        }
 		}
 	}
 }
